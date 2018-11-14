b349e3bfa789575c6121b54617f3a5dd89bcc91f
pirate765 <tusharblogger12@gmail.com>
2018-11-14 10:38:02 +0530

    Third Commit

--- src/howdyhighlands/settings.py
+++ src/howdyhighlands/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/pirate765.pythonanywhere.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if DEBUG:
     MEDIA_URL = '/media/'
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
