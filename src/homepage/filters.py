e66ef6dfd0d8f4c24b1ebc49099c176a31c6c66c
pirate765 <30777506+pirate765@users.noreply.github.com>
2018-12-08 13:49:28 +0530

    Update filters.py

--- src/homepage/filters.py
+++ src/homepage/filters.py
@@ -1,10 +1,1 @@
-from django.contrib.auth.models import User
-import django_filters
-from .models import Destinationpackage
 
-class PackageFilter(django_filters.FilterSet):
-    class Meta:
-        model = Destinationpackage
-        fields = {
-            'title':['icontains',],
-        }
